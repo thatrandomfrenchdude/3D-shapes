9c66b0cc8f81056947f60d804147a38af1303631
Nicholas Debeurre <debeurren@gmail.com>
2023-08-12 09:24:36 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 8080,
+    open: true,
+  },
+};
